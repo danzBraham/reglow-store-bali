edd55e1b1b06b22a1de0ee02f806873d9ffcb307
Zidan Abraham <zidangit07@gmail.com>
2023-12-17 15:23:27 +0800

    feat(components): implement Manfaat component

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,16 @@ export const NAV_LINKS: NavLink[] = [
   { key: "testimoni", href: "#testimoni", label: "Testimoni" },
   { key: "paket", href: "#paket", label: "Paket" },
 ];
+
+interface Manfaat {
+  no: number;
+  manfaat: string;
+}
+
+export const MANFAAT: Manfaat[] = [
+  { no: 1, manfaat: "Mengatasi masalah jerawat dan kemerahan" },
+  { no: 2, manfaat: "Menyamarkan kekusaman kulit" },
+  { no: 3, manfaat: "Menjaga kelembaban kulit" },
+  { no: 4, manfaat: "Membuat kulit tampak lebih muda" },
+  { no: 5, manfaat: "Produk yang cocok untuk kulit sensitif tanpa iritasi" },
+];
