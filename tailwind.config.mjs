b93eb38c5c7ebcc77a74e4461e73d299761fde85
Zidan Abraham <zidangit07@gmail.com>
2023-12-18 09:10:57 +0800

    fix(styles): adjust Hero component layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      screen: {
+      screens: {
         wide: "1440px",
       },
       fontFamily: {
