1210d335c564107872928695aeb4370c9ddf3626
Zidan Abraham <zidangit07@gmail.com>
2023-12-21 21:35:01 +0800

    chore(package): remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   i18n: {
     defaultLocale: "id",
-    locales: ["id"]
+    locales: ["id"],
   },
-  output: "server",
-  adapter: vercel()
 });
