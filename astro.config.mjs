ad78d44f6d35f55aa108b0b395497b7db7da6219
Zidan Abraham <zidangit07@gmail.com>
2023-12-21 20:06:30 +0800

    chore(config): update astro.config.mjs, bun.lockb, and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   i18n: {
     defaultLocale: "id",
-    locales: ["id"]
-  }
+    locales: ["id"],
+  },
 });
