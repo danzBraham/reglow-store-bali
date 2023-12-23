f7384f102d430b43a17b348feb3f5f46317157b1
Zidan Abraham <zidangit07@gmail.com>
2023-12-23 09:56:37 +0800

    feat(SEO): improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.reglowstorebali.com/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
+    sitemap(),
   ],
   i18n: {
     defaultLocale: "id",
