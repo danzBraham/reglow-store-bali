e9fd17742686271b9ac852f8728ee491ece251a3
Zidan Abraham <zidangit07@gmail.com>
2024-01-02 07:32:09 +0800

    chore: remove @astrojs/vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: "https://www.reglowstorebali.com/",
   integrations: [
     tailwind({
@@ -18,10 +18,4 @@ export default defineConfig({
     defaultLocale: "id",
     locales: ["id"],
   },
-  output: "static",
-  adapter: vercel({
-    speedInsights: {
-      enabled: true,
-    },
-  }),
 });
